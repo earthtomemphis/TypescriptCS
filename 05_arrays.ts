e1134caff580978c12b383c9633a65d07fe63aa0
Memphis DiAngelis <earthtomemphis@gmail.com>
2025-04-01 14:33:53 -0600

    array type

---
+++ 05_arrays.ts
@@ -0,0 +1,26 @@
+// const activeUsers: [] = [1];
+// activeUsers.push('steven');
+// const activeUsers: string[]=[]
+const activeUsers: string[] = ['colt'];
+activeUsers.push('Tony');
+// activeUsers.push(12);
+const ageList: number[] = [45, 56, 13];
+ageList[0] = 99;
+// ageList[0] = 'asd';
+const bools: Array<boolean> = [];
+const bools2: boolean[] = [];
+type Point2 = {
+	x: number;
+	y: number;
+};
+
+const coords: Point2[] = [];
+coords.push({ x: 23, y: 8 });
+// coords.push({y:8})
+// coords.push({x:23,y:'8'})
+
+const board: string[][] = [
+	['X', 'O', 'X'],
+	['X', 'O', 'X'],
+	['X', 'O', 'X'],
+];
